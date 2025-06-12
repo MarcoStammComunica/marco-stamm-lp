d33cd949a9a11cdb3bc969b53cd7917aad22d841
Marco Stamm <marco.stammm@gmail.com>
2025-06-12 11:05:44 -0400

    add Meta pixel to layout for tracking

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Marco Stamm Comunicação - Consultoria em Comunicação Pública',
+  description: 'Transformo sua equipe em um departamento de comunicação que funciona de verdade. Método comprovado com resultados em 30 dias.',
+  keywords: 'comunicação pública, consultoria municipal, gestão pública, assessoria de imprensa',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1048224067407141');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1048224067407141&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
